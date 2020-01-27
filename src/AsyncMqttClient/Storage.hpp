45f0aa342f5f46f9a3279e414f0a653a6b4bc1e7
mknjc <markus.knetschke@gmail.com>
2020-01-27 19:18:18 +0100

    fix fail

--- src/AsyncMqttClient/Storage.hpp
+++ src/AsyncMqttClient/Storage.hpp
@@ -10,12 +10,11 @@ struct PendingPubRel {
 
 struct AckPacket {
     AckPacket(PacketType type, uint8_t flags, uint16_t packetId) :
-            flags(flags), type(type), payloadSize(3), packetIdSize(2), packetId(packetId) {}
+            flags(flags), type(type), payloadSize(2), packetId(packetId) {}
 
     uint8_t flags : 4;
     PacketType type : 4;
-    uint8_t payloadSize; // normally 3
-    uint8_t packetIdSize; // normally 2
+    uint8_t payloadSize; // normally 2
     uint16_t packetId;
 } __attribute__((packed));
 
